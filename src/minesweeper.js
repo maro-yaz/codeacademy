e30f88fd654e1f483943b16806e49d82766b3a32
Marwane Yazback <myazback@homeaway.com>
2017-11-07 16:02:28 -0600

    initial commit minesweeper

---
+++ src/minesweeper.js
@@ -0,0 +1,11 @@
+const blankLine = '  |   |  ';
+console.log('This is what an empty board look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+const guessLine ='1 |   |  ';
+console.log('This is what a board with a guess and a bomb would look like:');
+const bombLine = '  | B |  ';
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
